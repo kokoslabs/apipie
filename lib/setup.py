c1755b349d1ffd209141e8f1916d59ba55c746b3
Ethan <175902687+kokorocks@users.noreply.github.com>
2025-07-31 12:17:53 -0400

    Update setup.py

--- lib/setup.py
+++ lib/setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="apipie-proxy",  # MUST be unique on PyPI
-    version="0.4.4",
-    description="A simple API proxy with rate limiting and API key support.",
+    name="apipie",  # MUST be unique on PyPI
+    version="0.4.51",
+    description="A simple API proxy with rate limiting and API key support for api organization.",
     author="kokorocks",
     packages=find_packages(),  # No 'where="src"' needed
     install_requires=[
